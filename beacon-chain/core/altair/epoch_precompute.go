199edce6789d90f8664fedb32d55bed64f211dbf
terence tsao <terence@prysmaticlabs.com>
2021-08-30 09:32:27 -0700

    ProcessEpochParticipation process only active validators

--- beacon-chain/core/altair/epoch_precompute.go
+++ beacon-chain/core/altair/epoch_precompute.go
@@ -96,10 +96,6 @@ func ProcessInactivityScores(
 			// Decrease inactivity score when validator gets target correct.
 			if v.InactivityScore > 0 {
 				score := uint64(1)
-				// Prevents underflow below 0.
-				if score > v.InactivityScore {
-					score = v.InactivityScore
-				}
 				v.InactivityScore -= score
 			}
 		} else {
@@ -126,6 +122,14 @@ func ProcessInactivityScores(
 
 // ProcessEpochParticipation processes the epoch participation in state and updates individual validator's pre computes,
 // it also tracks and updates epoch attesting balances.
+// Spec code:
+// if epoch == get_current_epoch(state):
+//        epoch_participation = state.current_epoch_participation
+//    else:
+//        epoch_participation = state.previous_epoch_participation
+//    active_validator_indices = get_active_validator_indices(state, epoch)
+//    participating_indices = [i for i in active_validator_indices if has_flag(epoch_participation[i], flag_index)]
+//    return set(filter(lambda index: not state.validators[index].slashed, participating_indices))
 func ProcessEpochParticipation(
 	ctx context.Context,
 	state state.BeaconState,
@@ -144,7 +148,7 @@ func ProcessEpochParticipation(
 	sourceIdx := cfg.TimelySourceFlagIndex
 	headIdx := cfg.TimelyHeadFlagIndex
 	for i, b := range cp {
-		if HasValidatorFlag(b, targetIdx) {
+		if HasValidatorFlag(b, targetIdx) && vals[i].IsActivePrevEpoch {
 			vals[i].IsCurrentEpochTargetAttester = true
 		}
 	}
@@ -153,13 +157,13 @@ func ProcessEpochParticipation(
 		return nil, nil, err
 	}
 	for i, b := range pp {
-		if HasValidatorFlag(b, sourceIdx) {
+		if HasValidatorFlag(b, sourceIdx) && vals[i].IsActiveCurrentEpoch {
 			vals[i].IsPrevEpochAttester = true
 		}
-		if HasValidatorFlag(b, targetIdx) {
+		if HasValidatorFlag(b, targetIdx) && vals[i].IsActiveCurrentEpoch {
 			vals[i].IsPrevEpochTargetAttester = true
 		}
-		if HasValidatorFlag(b, headIdx) {
+		if HasValidatorFlag(b, headIdx) && vals[i].IsActiveCurrentEpoch {
 			vals[i].IsPrevEpochHeadAttester = true
 		}
 	}
